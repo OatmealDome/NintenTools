5463e70aa2606259c15a0bcce2f68a86513b7539
Ray Koopa <RayKoopa@users.noreply.github.com>
2016-05-22 20:50:46 +0200

    Cleaning and optimizing BinaryReader a little

--- Blender/io_scene_bfres/binary_io.py
+++ Blender/io_scene_bfres/binary_io.py
@@ -16,17 +16,11 @@ class BinaryReader:
     def close(self):
         self.reader.close()
 
-    def read_boolean(self):
-        return struct.unpack(self.endianness + "B", self.reader.read(1))[0]
-
     def read_byte(self):
-        return struct.unpack(self.endianness + "B", self.reader.read(1))[0]
+        return self.reader.read(1)[0]
 
     def read_bytes(self, count):
         return self.reader.read(count)
-
-    def read_int16(self):
-        return struct.unpack(self.endianness + "h", self.reader.read(2))[0]
 
     def read_int32(self):
         return struct.unpack(self.endianness + "i", self.reader.read(4))[0]
@@ -48,19 +42,6 @@ class BinaryReader:
 
     def read_uint32s(self, count):
         return struct.unpack(self.endianness + str(int(count)) + "I", self.reader.read(4 * count))
-
-    def read_matrix3x3(self):
-        matrix = mathutils.Matrix()
-        matrix[0][0] = self.read_single()
-        matrix[0][1] = self.read_single()
-        matrix[0][2] = self.read_single()
-        matrix[1][0] = self.read_single()
-        matrix[1][1] = self.read_single()
-        matrix[1][2] = self.read_single()
-        matrix[2][0] = self.read_single()
-        matrix[2][1] = self.read_single()
-        matrix[2][2] = self.read_single()
-        return matrix
 
     def read_matrix4x3(self):
         matrix = mathutils.Matrix()
@@ -88,10 +69,6 @@ class BinaryReader:
             text += chr(i)
             i = self.read_byte()
         return text
-
-    def read_string(self):
-        length = self.read_uint32()
-        return self.reader.read(length * 2).decode("utf-16")
 
     def read_vector2f(self):
         return self.read_single(), self.read_single()
